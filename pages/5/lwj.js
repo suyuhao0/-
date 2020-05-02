2516678cc0a399be9836d31a58e468dfedc7fbd7
linwenjie <linwenjie@555.com>
2020-05-02 08:34:55 +0800

    添加了我的页面

--- pages/5/lwj.js
+++ pages/5/lwj.js
@@ -4,113 +4,138 @@ Page({
    * 页面的初始数据
    */
   data: {
-        arr:[
-             {"title":"代付款","img":"./imges/tu_10.jpg"},
-             {"title":"代成团","img":"./imges/tu_12.jpg"},
-             {"title":"代发货","img":"./imges/tu_14.jpg"},
-             {"title":"代收货","img":"./imges/tu_16.jpg"},
-             {"title":"售后服务","img":"./imges/tu_18.jpg"},
-        ],
-        arr1:[
-          {"title":"优惠券","img":"./imges/start_03.jpg"},
-          {"title":"完币","img":"./imges/start_06.jpg"},
-          {"title":"vip","img":"./imges/start_08.jpg"},
-          {"title":"礼物","img":"./imges/start_11.jpg"},
-          {"title":"收藏","img":"./imges/start_17.jpg"},
-          {"title":"好物星探","img":"./imges/start_18.jpg"},
-          {"title":"地址","img":"./imges/start_20.jpg"},
-          {"title":"客服","img":"./imges/start_22.jpg"},
-          {"title":"意见反馈","img":"./imges/start_27.jpg"},
-          {"title":"关于我们","img":"./imges/start_28.jpg"},
-        ],
-        list:[
-          {
-            "img":"./imges/list_03.jpg",
-            "name":"美的·小巧便捷轻松除尘的无线手持车载系···",
-            "connect":"西力克大6kpA，轻松除毛发灰尘，min小巧，随时···",
-            "price":399,
-            "oldprice":899
-          },
-          {
-            "img":"./imges/list_06.jpg",
-            "name":"深层清洁口腔牙缝呵护牙龈的环保牙线棒···",
-            "connect":"柔韧制裁，呵护牙龈，深层清洁牙齿，环保材质···",
-            "price":69,
-            "oldprice":899
-          },
-          {
-            "img":"./imges/can_03.jpg",
-            "name":"美的·小巧便捷轻松除尘的无线手持车载系···",
-            "connect":"西力克大6kpA，轻松除毛发灰尘，min小巧，随时···",
-            "price":399,
-            "oldprice":899
-          },
-          {
-            "img":"./imges/can_06.jpg",
-            "name":"美的·小巧便捷轻松除尘的无线手持车载系···",
-            "connect":"西力克大6kpA，轻松除毛发灰尘，min小巧，随时···",
-            "price":399,
-            "oldprice":899
-          },
-          {
-            "img":"./imges/can_10.jpg",
-            "name":"美的·小巧便捷轻松除尘的无线手持车载系···",
-            "connect":"西力克大6kpA，轻松除毛发灰尘，min小巧，随时···",
-            "price":399,
-            "oldprice":899
-          },
-          {
-            "img":"./imges/di_03.jpg",
-            "name":"美的·小巧便捷轻松除尘的无线手持车载系···",
-            "connect":"西力克大6kpA，轻松除毛发灰尘，min小巧，随时···",
-            "price":399,
-            "oldprice":899
-          },
-          {
-            "img":"./imges/di_06.jpg",
-            "name":"美的·小巧便捷轻松除尘的无线手持车载系···",
-            "connect":"西力克大6kpA，轻松除毛发灰尘，min小巧，随时···",
-            "price":399,
-            "oldprice":899
-          },
-          {
-            "img":"./imges/di_10.jpg",
-            "name":"美的·小巧便捷轻松除尘的无线手持车载系···",
-            "connect":"西力克大6kpA，轻松除毛发灰尘，min小巧，随时···",
-            "price":399,
-            "oldprice":899
-          },
-          {
-            "img":"./imges/di_11.jpg",
-            "name":"美的·小巧便捷轻松除尘的无线手持车载系···",
-            "connect":"西力克大6kpA，轻松除毛发灰尘，min小巧，随时···",
-            "price":399,
-            "oldprice":899
-          },
-          {
-            "img":"./imges/kai_03.jpg",
-            "name":"美的·小巧便捷轻松除尘的无线手持车载系···",
-            "connect":"西力克大6kpA，轻松除毛发灰尘，min小巧，随时···",
-            "price":399,
-            "oldprice":899
-          },
-          {
-            "img":"./imges/kai_10.jpg",
-            "name":"美的·小巧便捷轻松除尘的无线手持车载系···",
-            "connect":"西力克大6kpA，轻松除毛发灰尘，min小巧，随时···",
-            "price":399,
-            "oldprice":899
-          },
-        ]
+    count: 0,
+    page: 4,
+    height: 700,
+    loading: true,
+
+    arr: [{
+        "title": "代付款",
+        "img": "./imges/tu_10.jpg"
+      },
+      {
+        "title": "代成团",
+        "img": "./imges/tu_12.jpg"
+      },
+      {
+        "title": "代发货",
+        "img": "./imges/tu_14.jpg"
+      },
+      {
+        "title": "代收货",
+        "img": "./imges/tu_16.jpg"
+      },
+      {
+        "title": "售后服务",
+        "img": "./imges/tu_18.jpg"
+      },
+    ],
+    arr1: [{
+        "title": "优惠券",
+        "img": "./imges/start_03.jpg"
+      },
+      {
+        "title": "完币",
+        "img": "./imges/start_06.jpg"
+      },
+      {
+        "title": "vip",
+        "img": "./imges/start_08.jpg"
+      },
+      {
+        "title": "礼物",
+        "img": "./imges/start_11.jpg"
+      },
+      {
+        "title": "收藏",
+        "img": "./imges/start_17.jpg"
+      },
+      {
+        "title": "好物星探",
+        "img": "./imges/start_18.jpg"
+      },
+      {
+        "title": "地址",
+        "img": "./imges/start_20.jpg"
+      },
+      {
+        "title": "客服",
+        "img": "./imges/start_22.jpg"
+      },
+      {
+        "title": "意见反馈",
+        "img": "./imges/start_27.jpg"
+      },
+      {
+        "title": "关于我们",
+        "img": "./imges/start_28.jpg"
+      },
+    ],
+    list: [],
+    ff: true
+
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    let that = this
+    let url = "https://suyuhao007.gitee.io/datajson/product_data.json"
+    wx.request({
+      url: url,
+      method: "GET",
+      success: (res) => {
+        console.log(res)
+        if (res.statusCode == 200) {
+          that.setData({
+            list: res.data.result.splice(this.data.count, this.data.page)
+          })
+        }
+      }
 
+    })
   },
 
+  down() {
+    if (this.data.ff) {
+      wx.showLoading()
+      wx.getSystemInfo({
+        success: (res) => {
+          this.setData({
+            height: res.windowHeight,
+            ff: false
+          })
+        }
+      })
+      wx.request({
+        url: "https://suyuhao007.gitee.io/datajson/product_data.json",
+        method: "GET",
+        success: (res) => {
+          let count = this.data.count + 4
+          let page = this.data.page + 4
+          var ll = res.data.result.slice(count, page);
+          ll.forEach((v, k) => {
+            this.data.list.push(v);
+          });
+          if (res.statusCode == 200) {
+            this.setData({
+              count: count,
+              page: page,
+              list: this.data.list,
+              ff: true,
+            })
+            wx.hideLoading()
+          }
+        }
+
+      })
+
+      console.log("到低部了")
+    }
+
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
